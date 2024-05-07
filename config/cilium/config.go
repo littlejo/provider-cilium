dc58694e8938bd92c4250e670bde993d4fd667d2
Joseph Ligier <joseph.ligier@accenture.com>
2024-05-07 18:09:19 +0200

    Add cilium_deploy

--- config/cilium/config.go
+++ config/cilium/config.go
@@ -4,9 +4,9 @@ import "github.com/crossplane/upjet/pkg/config"
 
 // Configure configures individual resources by adding custom ResourceConfigurators.
 func Configure(p *config.Provider) {
-	p.AddResourceConfigurator("cilium", func(r *config.Resource) {
+	p.AddResourceConfigurator("cilium_deploy", func(r *config.Resource) {
 		// We need to override the default group that upjet generated for
 		// this resource, which would be "cilium"
-		r.ShortGroup = "cilium"
+		r.ShortGroup = "core"
 	})
 }
